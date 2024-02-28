98b6092cb64037e2b0c738736627a0b2af3ee40c
lucasbattistipiva <142608168+lucasbattistipiva@users.noreply.github.com>
2024-02-28 09:10:08 -0300

    Add files via upload
    Improvement HUD

--- prog.js
+++ prog.js
@@ -1,6 +1,9 @@
 let resposta = document.getElementById('resposta');
 let sortear = document.getElementById('sortear');
+let numbers = []
 
 sortear.addEventListener('click',function(){
+    const ball = document.querySelector("bolas");
+    numbers.push(ball)
 
 });
